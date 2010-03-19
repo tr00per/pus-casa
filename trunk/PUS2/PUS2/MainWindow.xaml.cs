4ed4d4e33286ee57fdfc29de55eed78b3dd7367e
arturczajka <arturczajka@f056a0d6-22c5-11df-a494-6dfd6523cf1c>
2010-03-19 10:31:18 +0000

    Komunikacja między klientem i serwerem istnieje. Trzeba uzupełnić funkcjonalność programu samą w sobie.

--- trunk/PUS2/PUS2/MainWindow.xaml.cs
+++ trunk/PUS2/PUS2/MainWindow.xaml.cs
@@ -22,13 +22,18 @@ namespace PUS2
         TcpClient client;
         TcpListener server;
         System.Text.Encoding encoding;
+        Thread serverThread;
 
         const Int32 queryPort = 4000;
         const int bufferSize = 512;
 
+        delegate void LogDelegate(String msg);
+        LogDelegate log;
+        
         public void Log(String msg)
         {
             Output.AppendText(msg + "\r\n");
+            Output.ScrollToEnd();
         }
 
         public Main()
@@ -36,17 +41,64 @@ namespace PUS2
             InitializeComponent();
         }
 
-        private void InitializeNetwork(object sender, RoutedEventArgs e)
+        private void Initialize(object sender, RoutedEventArgs e)
         {
             Status.Content = "Ready!";
             client = null;
             server = null;
             encoding = new System.Text.ASCIIEncoding();
+            log = new LogDelegate(Log);
+        }
+
+        private void runServer()
+        {
+            server = new TcpListener(IPAddress.Any, queryPort);
+            server.Start();
+
+            byte[] buffer = new byte[bufferSize];
+
+            while (true)
+            {
+                if (server.Pending())
+                {
+                    client = server.AcceptTcpClient();
+                    this.Dispatcher.Invoke(log, (object)("Client accepted: " + client.Client.LocalEndPoint.ToString()));
+
+                    NetworkStream stream = client.GetStream();
+
+                    int trans = stream.Read(buffer, 0, bufferSize);
+                    String query = encoding.GetString(buffer, 0, trans);
+
+                    this.Dispatcher.Invoke(log, (object)("Got queried: " + query));
+
+                    byte[] response = GatherOutputdata(query);
+
+                    stream.Write(response, 0, response.Length);
+
+                    stream.Close();
+                    client.Close();
+                    client = null;
+
+                    this.Dispatcher.Invoke(log, (object)("Client got served."));
+
+                    if (query.Equals("~~kill"))
+                    {
+                        this.Dispatcher.Invoke(log, (object)("Exiting..."));
+                        break;
+                    }
+                }
+
+            }
+
+            server.Stop();
+            server = null;
+
+            this.Dispatcher.Invoke(log, (object)("Server stopped."));
         }
 
         private void ServerStart_Click(object sender, RoutedEventArgs e)
         {
-            if (server != null)
+            if (server != null && serverThread.IsAlive)
             {
                 return;
             }
@@ -57,46 +109,21 @@ namespace PUS2
                 client = null;
             }
 
-            server = new TcpListener(IPAddress.Any, queryPort);
+            Log("Spawning server...");
 
-            server.Start();
-
-            bool running = true;
-            byte[] buffer = new byte[bufferSize];
-
-            while (running)
-            {
-                if (server.Pending())
-                {
-                    client = server.AcceptTcpClient();
-                    Log("Client accepted: " + client.ToString());
-
-                    NetworkStream stream = client.GetStream();
-                    String query = encoding.GetString(buffer);
-
-                    stream.Read(buffer, 0, bufferSize);
-                    
-                    Log("Got queried: " + query);
-
-                    byte[] response = GatherOutputdata(query);
-
-                    stream.Write(response, 0, response.Length);
-
-                    stream.Close();
-
-                    Log("Client got served.");
-                }                
-            }
-
-            server.Stop();
-            server = null;
-
-            Log("Server stopped.");
+            serverThread = new Thread(new ThreadStart(runServer));
+            serverThread.Start();
         }
 
         private byte[] GatherOutputdata(string query)
         {
-            byte[] ret = {(byte)'A', (byte)'B', (byte)'C'};
+            if (query.Equals("~~kill"))
+            {
+                return encoding.GetBytes("SHD");
+            }
+
+            //FIXME wstawić działanie programu (na podstawie poprzedniej wersji)
+            byte[] ret = encoding.GetBytes("ABC");
             return ret;
         }
 
@@ -105,7 +132,7 @@ namespace PUS2
             if (server != null)
             {
                 //TODO inform pending clients that fun is over
-                server.Stop();
+                serverThread.Interrupt();
                 server = null;
             }
 
@@ -129,11 +156,12 @@ namespace PUS2
 
             Log("Awaiting response...");
             byte[] buffer = new byte[bufferSize];
-            while (stream.DataAvailable)
+            do
             {
-                stream.Read(buffer, 0, bufferSize);
-                Output.AppendText(encoding.GetString(buffer));
-            }
+                int trans = stream.Read(buffer, 0, bufferSize);
+                Output.AppendText(encoding.GetString(buffer, 0, trans));
+            } while (stream.DataAvailable);
+            Output.ScrollToEnd();
 
             stream.Close();
 
@@ -141,6 +169,14 @@ namespace PUS2
             client = null;
 
             Log("\r\nConnection ended.");
+        }
+
+        private void Finish(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (serverThread.IsAlive)
+            {
+                serverThread.Interrupt();
+            }
         }
     }
 }
