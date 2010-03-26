b9eb0a14ef386b2ffd1d69467dee975a896efd4a
andrzej.simin <andrzej.simin@f056a0d6-22c5-11df-a494-6dfd6523cf1c>
2010-03-26 13:07:35 +0000

    Pozostały szczegóły typu wyświetlanie komentarzy, czy zmiana nazwy formsów.
    Poza tym - działa :)

---
+++ 3/RemoteClass/RemoteClass.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+
+namespace RemoteClass
+{
+    public class RemoteObject: MarshalByRefObject
+    {
+        System.Text.Encoding encoding;
+        string serverRoot;
+        RemoteObject()
+        {
+            encoding = new System.Text.ASCIIEncoding();
+            serverRoot = Directory.GetCurrentDirectory();
+        }
+
+        public string getFile(string tekst)
+        {
+            this.InitializeLifetimeService();
+            return encoding.GetString(GatherOutputData(tekst));
+
+        }
+
+        private byte[] GatherOutputData(string query)
+        {
+            byte[] buf = new byte[1024];
+            string ret = "";
+            string path = serverRoot + query;
+
+
+            if (File.Exists(path))
+            {
+                FileStream fin = File.OpenRead(path);
+
+                int read;
+                do
+                {
+                    read = fin.Read(buf, 0, 1024);
+                    ret += encoding.GetString(buf, 0, read);
+                } while (read == 1024);
+            }
+            else
+            {
+                if (!Directory.Exists(path))
+                {
+                    path = serverRoot;
+                }
+
+                string[] dirs = Directory.GetDirectories(path);
+                foreach (string dir in dirs)
+                {
+                    ret += dir + "\\\r\n";
+                }
+                string[] files = Directory.GetFiles(path);
+                foreach (string file in files)
+                {
+                    ret += file + "\r\n";
+                }
+            }
+            
+
+            return encoding.GetBytes(ret);
+        }
+
+
+    }
+}
---
+++ 3/Server/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(312, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            999999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.numericUpDown1.Size = new System.Drawing.Size(150, 20);
+            this.numericUpDown1.TabIndex = 0;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            4000,
+            0,
+            0,
+            0});
+            // 
+            // listBox1
+            // 
+            this.listBox1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(0, 0);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(306, 160);
+            this.listBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(312, 80);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 57);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 160);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
