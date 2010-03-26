486609eb849a416a53de763fce64a4e7cd5361b4
andrzej.simin <andrzej.simin@f056a0d6-22c5-11df-a494-6dfd6523cf1c>
2010-03-26 13:07:35 +0000

    Pozostały szczegóły typu wyświetlanie komentarzy, czy zmiana nazwy formsów.
    Poza tym - działa :)

---
+++ trunk/3/Klient/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using RemoteClass;
+using System.Runtime.Remoting.Channels.Http;
+using System.Runtime.Remoting.Channels;
+
+namespace Klient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string adres = textBox1.Text;
+            adres = "http://" + adres;
+            int port = (int)numericUpDown1.Value;
+            HttpClientChannel kanal = null;
+            try
+            {
+                kanal = new HttpClientChannel();
+                ChannelServices.RegisterChannel(kanal, false);
+                RemoteObject obiekt = (RemoteObject)Activator.GetObject(typeof(RemoteObject), adres + ":" + port.ToString() + "/NazwaUslugiIObiektu");
+                textBox3.AppendText(obiekt.getFile(textBox2.Text));
+                ChannelServices.UnregisterChannel(kanal);
+                textBox3.AppendText("polaczenie zostalo zakonczone");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "blad");
+                textBox3.AppendText("Blad polaczenia");
+                ChannelServices.UnregisterChannel(kanal);
+            }
+        }
+    }
+}
---
+++ trunk/3/Server/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using RemoteClass;
+using System.Runtime.Remoting.Channels;
+using System.Runtime.Remoting;
+using System.Runtime.Remoting.Channels.Http;
+
+
+namespace Server
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                HttpServerChannel kanal = new HttpServerChannel((int)numericUpDown1.Value);
+                ChannelServices.RegisterChannel(kanal, false);
+                
+                RemotingConfiguration.RegisterWellKnownServiceType(typeof(RemoteObject), "NazwaUslugiIObiektu", WellKnownObjectMode.Singleton);
+
+                listBox1.Items.Add("Serwer został uruchomiony.");
+                listBox1.Items.Add("   Port:" + this.numericUpDown1.Value.ToString());
+                listBox1.Items.Add("   IP: ");
+                IPAddress[] ipList = Dns.GetHostEntry(Dns.GetHostName()).AddressList;
+                foreach (IPAddress ip in ipList.Reverse())
+                {
+                    listBox1.Items.Add("        " + ip.ToString());
+                }
+            }
+            catch (Exception exc)
+            {
+                MessageBox.Show(exc.Message, "Blad");
+            }
+        }
+    }
+}
