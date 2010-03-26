02f89be23dd6e05d7e36116f11cf34d5771ce122
andrzej.simin <andrzej.simin@f056a0d6-22c5-11df-a494-6dfd6523cf1c>
2010-03-26 23:03:44 +0000

    zad4 :)

---
+++ 4/WebService/App_Code/Service.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.IO;
+
+[WebService(Namespace = "http://tempuri.org/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+// To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
+// [System.Web.Script.Services.ScriptService]
+public class Service : System.Web.Services.WebService
+{
+    System.Text.Encoding encoding;
+    string serverRoot;
+    public Service () {
+        encoding = new System.Text.ASCIIEncoding();
+        serverRoot = "C:\\Users\\Andrzej\\Documents\\Visual Studio 2008\\Projects\\PUS\\root";
+        //Uncomment the following line if using designed components 
+        //InitializeComponent(); 
+    }
+
+    [WebMethod]
+    public char[] getFile(string query) {
+        byte[] buf = new byte[1024];
+        string ret = "";
+        string path = serverRoot + query;
+
+
+        if (File.Exists(path))
+        {
+            FileStream fin = File.OpenRead(path);
+
+            int read;
+            do
+            {
+                read = fin.Read(buf, 0, 1024);
+                ret += encoding.GetString(buf, 0, read);
+            } while (read == 1024);
+        }
+        else
+        {
+            if (!Directory.Exists(path))
+            {
+                path = serverRoot;
+            }
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string dir in dirs)
+            {
+                ret += dir + "\\\r\n";
+            }
+            string[] files = Directory.GetFiles(path);
+            foreach (string file in files)
+            {
+                ret += file + "\r\n";
+            }
+        }
+        return encoding.GetChars(encoding.GetBytes(ret));
+    }
+    
+}
---
+++ 4/klient/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace klient
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                PUS.Service serwis = new klient.PUS.Service(); //utworzenie obiektu reprezentującego
+                //// zdalną usługę
+                textBox3.AppendText("\r\n=============\r\n");
+                textBox3.AppendText(new string(serwis.getFile(textBox1.Text))); // Wywołujemy zdalną metodę 
+                textBox3.AppendText("\r\n=============\r\n");
+            }
+            catch (Exception ex)				        
+            {
+                MessageBox.Show(ex.Message);
+            }
+
+        }
+    }
+}
