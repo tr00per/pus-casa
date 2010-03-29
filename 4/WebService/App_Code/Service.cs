f1ea1758194879da4d2a7f42eee2690f4d686fdf
andrzej.simin <andrzej.simin@f056a0d6-22c5-11df-a494-6dfd6523cf1c>
2010-03-29 07:24:26 +0000

    kosmetyczne znowu

--- 4/WebService/App_Code/Service.cs
+++ 4/WebService/App_Code/Service.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Services;
 using System.IO;
 
-[WebService(Namespace = "http://tempuri.org/")]
+[WebService(Namespace = "http://example.com")]
 [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
 // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line. 
 // [System.Web.Script.Services.ScriptService]
